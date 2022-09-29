31f475963b8aac7fcfa0a9404c1e5fa63e2d9024
Mr. Sableye <mister.sableye@gmail.com>
2022-09-29 02:06:12 -0500

    Adds scripts for copying files

---
+++ copy.bat
@@ -0,0 +1,3 @@
+xcopy ./clover/sprites ../clovermon-showdown-client/sprites /s /e
+xcopy ./clover/audio ../clovermon-showdown-client/audio /s /e
+xcopy ./clover-cap/sprites ../clovermon-showdown-client/sprites /s /e
---
+++ copy.sh
@@ -0,0 +1,3 @@
+cp -R ./clover/sprites/* ../clovermon-showdown-client/sprites/
+cp -R ./clover/audio/* ../clovermon-showdown-client/audio/
+cp -R ./clover-cap/sprites/* ../clovermon-showdown-client/sprites/
