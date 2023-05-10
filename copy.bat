0de946630ee1ee162c87185492c996c36dafb5a7
Mr. Sableye <mister.sableye@gmail.com>
2023-05-10 14:14:43 -0500

    Updates scripts to move audio

--- copy.bat
+++ copy.bat
@@ -1,3 +1,4 @@
 robocopy clover/sprites ../clovermon-showdown-client/sprites /e
 robocopy clover/audio ../clovermon-showdown-client/audio /e
 robocopy clover-cap/sprites ../clovermon-showdown-client/sprites /e
+robocopy clover-cap/audio ../clovermon-showdown-client/audio /e
