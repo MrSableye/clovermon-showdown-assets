a36111e233c6a865b99844d284c3b19b2c3be900
Mr. Sableye <mister.sableye@gmail.com>
2023-07-15 11:01:47 -0500

    Updates copy scripts

--- copy.bat
+++ copy.bat
@@ -2,3 +2,4 @@ robocopy clover/sprites ../clovermon-showdown-client/sprites /e
 robocopy clover/audio ../clovermon-showdown-client/audio /e
 robocopy clover-cap/sprites ../clovermon-showdown-client/sprites /e
 robocopy clover-cap/audio ../clovermon-showdown-client/audio /e
+robocopy wack/sprites ../clovermon-showdown-client/sprites /e
