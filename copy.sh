b41f056a2ac48e89b51d81c75cfc94facbe01481
Mr. Sableye <mister.sableye@gmail.com>
2023-09-17 01:07:46 -0500

    Adds Sburbmon sprites

--- copy.sh
+++ copy.sh
@@ -4,3 +4,4 @@ cp -R ./clover-cap/sprites/* ../clovermon-showdown-client/sprites/
 cp -R ./clover-cap/audio/* ../clovermon-showdown-client/audio/
 cp -R ./wack/sprites/* ../clovermon-showdown-client/sprites/
 cp -R ./wack/audio/* ../clovermon-showdown-client/audio/
+cp -R ./sburbmons/sprites/* ../clovermon-showdown-client/sprites/
