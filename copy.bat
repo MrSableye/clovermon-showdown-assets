267325e4517b8d1c3d1c25b74f80bee2df800f08
Mr. Sableye <mister.sableye@gmail.com>
2023-11-05 22:20:30 -0600

    Adds Pokemon Sweet sprites (non-megas)

--- copy.bat
+++ copy.bat
@@ -5,3 +5,4 @@ robocopy clover-cap/audio ../clovermon-showdown-client/audio /e
 robocopy wack/sprites ../clovermon-showdown-client/sprites /e
 robocopy wack/audio ../clovermon-showdown-client/audio /e
 robocopy sburbmons/sprites ../clovermon-showdown-client/sprites /e
+robocopy sweet/sprites ../clovermon-showdown-client/sprites /e
